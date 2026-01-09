4f42ca1e85a607dcdfbd22c3b4b753c44929d752
Gokul-Prabakran1 <165556566+Gokul-Prabakran1@users.noreply.github.com>
2026-01-09 09:41:53 +0530

    Delete frontend directory

--- frontend/src/components/ui/label.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Label = (props: React.LabelHTMLAttributes<HTMLLabelElement>) => {
-  return <label {...props}>{props.children}</label>;
-};
-
-export default Label;
